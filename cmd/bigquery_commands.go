dba4d66bb4e1fa197eea3254edea8680f6fd319b
magiodev <31893902+magiodev@users.noreply.github.com>
2023-05-18 20:22:00 +0200

    Fixing config. usages

--- cmd/bigquery_commands.go
+++ cmd/bigquery_commands.go
@@ -3,12 +3,13 @@ package cmd
 import (
 	"github.com/arhamchordia/chain-details/cmd/bigquery"
 	"github.com/arhamchordia/chain-details/cmd/bigquery/vaults"
+	"github.com/arhamchordia/chain-details/cmd/config"
 	"github.com/spf13/cobra"
 )
 
 func BigQueryRegisterRawQueryCmd(parentCmd *cobra.Command) {
 	// RawQuery
-	bigquery.RawQueryCmd.Flags().StringVarP(&bigquery.RawQuery, "query", "q", "", "SQL query to execute against BigQuery (required)")
+	bigquery.RawQueryCmd.Flags().StringVarP(&config.RawQuery, "query", "q", "", "SQL query to execute against BigQuery (required)")
 	err := bigquery.RawQueryCmd.MarkFlagRequired("query")
 	if err != nil {
 		return
@@ -18,7 +19,7 @@ func BigQueryRegisterRawQueryCmd(parentCmd *cobra.Command) {
 
 func BigQueryRegisterTransactionsCmd(parentCmd *cobra.Command) {
 	// TransactionsQuery
-	bigquery.TransactionsCmd.Flags().StringVarP(&bigquery.AddressQuery, "address", "a", "", "Address to query (required)")
+	bigquery.TransactionsCmd.Flags().StringVarP(&config.AddressQuery, "address", "a", "", "Address to query (required)")
 	err := bigquery.TransactionsCmd.MarkFlagRequired("address")
 	if err != nil {
 		return
@@ -28,18 +29,18 @@ func BigQueryRegisterTransactionsCmd(parentCmd *cobra.Command) {
 
 func BigQueryRegisterVaultsCmd(parentCmd *cobra.Command) {
 	// VaultBondQuery
-	vaults.BondCmd.Flags().StringVarP(&vaults.AddressQuery, "address", "a", "", "Address to query (optional)")
-	vaults.BondCmd.Flags().BoolVarP(&vaults.ConfirmedQuery, "confirmed", "c", false, "Filter by confirmed bond actions (optional)")
-	vaults.BondCmd.Flags().BoolVarP(&vaults.PendingQuery, "pending", "p", false, "Filter by pending bond actions (optional)")
+	vaults.BondCmd.Flags().StringVarP(&config.AddressQuery, "address", "a", "", "Address to query (optional)")
+	vaults.BondCmd.Flags().BoolVarP(&config.ConfirmedQuery, "confirmed", "c", false, "Filter by confirmed bond actions (optional)")
+	vaults.BondCmd.Flags().BoolVarP(&config.PendingQuery, "pending", "p", false, "Filter by pending bond actions (optional)")
 	parentCmd.AddCommand(vaults.BondCmd)
 
 	// VaultUnbondQuery
-	vaults.UnbondCmd.Flags().StringVarP(&vaults.AddressQuery, "address", "a", "", "Address to query (optional)")
-	vaults.UnbondCmd.Flags().BoolVarP(&vaults.ConfirmedQuery, "confirmed", "c", false, "Filter by confirmed unbond actions (optional)")
-	vaults.UnbondCmd.Flags().BoolVarP(&vaults.PendingQuery, "pending", "p", false, "Filter by pending unbond actions (optional)")
+	vaults.UnbondCmd.Flags().StringVarP(&config.AddressQuery, "address", "a", "", "Address to query (optional)")
+	vaults.UnbondCmd.Flags().BoolVarP(&config.ConfirmedQuery, "confirmed", "c", false, "Filter by confirmed unbond actions (optional)")
+	vaults.UnbondCmd.Flags().BoolVarP(&config.PendingQuery, "pending", "p", false, "Filter by pending unbond actions (optional)")
 	parentCmd.AddCommand(vaults.UnbondCmd)
 
 	// VaultWithdrawQuery
-	vaults.WithdrawCmd.Flags().StringVarP(&vaults.AddressQuery, "address", "a", "", "Address to query (optional)")
+	vaults.WithdrawCmd.Flags().StringVarP(&config.AddressQuery, "address", "a", "", "Address to query (optional)")
 	parentCmd.AddCommand(vaults.WithdrawCmd)
 }
